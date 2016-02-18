c862c834343368ee52e3098561bfeebda3701028
SamuelSahonero <ssahonero@gmail.com>
2016-02-17 21:17:15 -0400

    updated the table practice

---
+++ class exercises/table.js
@@ -0,0 +1,20 @@
+/**
+ * Created by SamuelSahonero on 2/17/2016.
+ */
+
+
+var createTable = function () {
+
+    var table = document.getElementById("myTable");
+
+// Create an empty <tr> element and add it to the 1st position of the table:
+    var row = table.insertRow(0);
+
+// Insert new cells (<td> elements) at the 1st position of the "new" <tr> element:
+    var cell = row.insertCell(0);
+
+// Add some text to the new cells:
+    cell1.innerHTML = "NEW CELL1";
+
+
+};
